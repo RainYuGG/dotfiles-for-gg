9295568752cb520289101c953019cb66b4830f74
Ching-Yu Chiang <r10922141@csie.ntu.edu.tw>
2026-06-06 05:18:07 +0800

    feat: add tpm init

--- init.sh
+++ init.sh
@@ -26,6 +26,12 @@ git config --global diff.colorMoved default
 # omz
 
 # tpm
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    echo "Installing TPM..."
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+echo "Installing tmux plugins..."
+~/.tmux/plugins/tpm/bin/install_plugins
 
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
